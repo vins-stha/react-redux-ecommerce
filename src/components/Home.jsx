ea4d09c955618615fd47d2d7bd95774c4d6fd77d
vins_stha <shrs_samar@yahoo.com>
2022-01-13 19:42:04 +0200

    Fixed #2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export const Home = () => {
     return (
         <div>
-            <h1>Home</h1>    
+            <h1>Home</h1>  
+            <h1>more on home component</h1>  
         </div>
     )
 }
