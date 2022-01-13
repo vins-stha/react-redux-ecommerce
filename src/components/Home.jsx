47155a783b15bba2b0094251f1d7961cdb9f0584
vins_stha <shrs_samar@yahoo.com>
2022-01-13 19:48:28 +0200

    Done #2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ export const Home = () => {
     return (
         <div>
             <h1>Home</h1>  
-            <h1>more on home component</h1>  
+            <h1>more on home </h1>  
         </div>
     )
 }
