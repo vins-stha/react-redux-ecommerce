4ef94985d6ae575266361568c9f580758e323648
vins_stha <shrs_samar@yahoo.com>
2022-01-14 17:40:07 +0200

    packages for routing updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import './App.css';
+
+import {BrowserRouter, Routes, NavLink, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom';
 import {createStore} from 'redux'
 import { Home } from './components/Home';
 import { Navbar } from './components/Navbar';
 import { Cart } from './components/Cart';
 import { UserLogin } from './components/UserLogin';
+import Products from './components/Product';
+import ProductDetail from './components/ProductDetail';
 function App() {
  
   
   
   return (
   
-    <div className='container m-t-5 text-white'>
+    <div className='container m-t-5'>
       
     <Navbar/> 
     <Home/>
-    <Cart/>
-    <UserLogin/>
+
+    <BrowserRouter>
+      <Routes>
+        {/* <Route exact path="/" element= {<Home/>}/> */}
+        <Route exact path="/products" element= {<Products/>}/>
+        <Route exact path="/product/:id" element= {<ProductDetail/>}/>
+
+      </Routes>
+    </BrowserRouter>
+    {/* <Cart/>
+    <UserLogin/> */}
+    {/* <Product/>
+    <ProductDetail/> */}
+    {/* <BrowserRouter>
+      <Switch>
+       
+          <Route exact path="/product/:id" element={<ProductDetail/>}></Route>
+       
+      </Switch>
+    </BrowserRouter>  */}
     </div>
     
    
